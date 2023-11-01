15f268bfdd42d568df6f50473e1347c0914b9628
Huy Hoang <boydaihungst@gmail.com>
2023-11-02 00:58:12 +0700

    Initial

---
+++ derpfest.mk
@@ -0,0 +1,28 @@
+$(call inherit-product, vendor/derp/config/common_full_phone.mk)
+$(call inherit-product, vendor/derp/config/BoardConfigDerpFest.mk)
+$(call inherit-product, device/derp/sepolicy/common/sepolicy.mk)
+-include vendor/derp/build/core/config.mk
+
+TARGET_HAS_FUSEBLK_SEPOLICY_ON_VENDOR := true
+TARGET_USES_PREBUILT_VENDOR_SEPOLICY := true
+SELINUX_IGNORE_NEVERALLOWS := true
+BOARD_EXT4_SHARE_DUP_BLOCKS := true
+
+BUILD_BROKEN_DUP_RULES := true
+
+TARGET_BOOT_ANIMATION_RES := 1080
+# Currently haven't been supported yet
+# EXTRA_UDFPS_ANIMATIONS := true
+# TARGET_FACE_UNLOCK_SUPPORTED := true
+
+TARGET_NO_KERNEL_OVERRIDE := true
+TARGET_NO_KERNEL_IMAGE := true
+
+DERP_BUILDTYPE := CI
+
+BOARD_USES_QCOM_HARDWARE := true
+
+OVERRIDE_OTA_CHANNEL := https://raw.githubusercontent.com/boydaihungst/treble_build_derpfest/A14/ota.json
+
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    ro.system.ota.json_url=https://raw.githubusercontent.com/boydaihungst/treble_build_derpfest/A14/ota.json
